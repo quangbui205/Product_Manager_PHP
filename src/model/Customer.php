202fd72dfb382b0da1b6da255e760373032ed230
quanxuctu <quanhvgch15231@fpt.edu.vn>
2020-06-11 10:40:23 +0700

    update add customer

---
+++ src/model/Customer.php
@@ -0,0 +1,99 @@
+<?php
+namespace File\model;
+
+class Customer{
+    protected $id;
+    protected $name;
+    protected $email;
+    protected $phone;
+    protected $address;
+
+    public function __construct($id, $name, $email, $phone, $address)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->phone = $phone;
+        $this->address = $address;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param mixed $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param mixed $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+}
---
+++ src/view/customers/addCustomer.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form method="post">
+    <table class="table table-hover col-12 col-md-6">
+        <tr>
+            <th>ID:</th>
+            <td><input type="text" name="id" required autofocus class="col-12"></td>
+        </tr>
+        <tr>
+            <th>Name:</th>
+            <td><input type="text" name="name" required autofocus class="col-12"></td>
+        </tr>
+        <tr>
+            <th>Email:</th>
+            <td><input type="text" name="email" required autofocus class="col-12"></td>
+        </tr>
+        <tr>
+            <th>Phone:</th>
+            <td><input type="text" name="phone" required autofocus class="col-12"></td>
+        </tr>
+        <tr>
+            <th>Address:</th>
+            <td><input type="text" name="address" required autofocus class="col-12"></td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <button type="submit" class="btn btn-success">ADD</button>
+            </td>
+        </tr>
+    </table>
+</form>
+</body>
+</html>
+
